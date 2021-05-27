35733865684b768e2fef44695c53a51db6632850
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-05-27 16:45:03 -0300

    :zap: add list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  deleteCategory(id: string): void {
+    alert(id);
   }
 
 }
