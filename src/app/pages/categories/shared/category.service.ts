345ccc54b5c4b1c71690036887dcb2f23a88ebeb
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-06-01 14:54:42 -0300

    :zap: improving BaseResourceService and Refactoring CategoryService

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,67 +1,14 @@
-import {Injectable} from '@angular/core';
+import {Injectable, Injector} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
-import {Observable, throwError} from 'rxjs';
-import {map, catchError} from 'rxjs/operators';
-
+import {BaseResourceService} from '../../../shared/services/base-resource.service';
 import {CategoryModel} from './category.model';
 import {environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
-  constructor(private http: HttpClient) {
-  }
-
-  private static handleError(error: any): Observable<any> {
-    console.error(`Erro na requisição -> ${error}`);
-    return throwError(error);
-  }
-
-  getAll(): Observable<CategoryModel[]> {
-    return this.http.get(`${environment.baseUrl}/categories`).pipe(
-      catchError(CategoryService.handleError),
-      map(this.jsonDataToCategories)
-    );
-  }
-
-  getById(id: number): Observable<CategoryModel> {
-    return this.http.get(`${environment.baseUrl}/categories/${id}`).pipe(
-      catchError(CategoryService.handleError),
-      map(this.jsonDataToCategory)
-    );
-  }
-
-  create(category: CategoryModel): Observable<CategoryModel> {
-    return this.http.post(`${environment.baseUrl}/categories`, category).pipe(
-      catchError(CategoryService.handleError),
-      map(() => category)
-    );
-  }
-
-  update(category: CategoryModel): Observable<CategoryModel> {
-    return this.http.put(`${environment.baseUrl}/categories/${category.id}`, category).pipe(
-      catchError(CategoryService.handleError),
-      map(this.jsonDataToCategory)
-    );
-  }
-
-  delete(category: CategoryModel): Observable<any> {
-    return this.http.delete(`${environment.baseUrl}/categories/${category.id}`).pipe(
-      catchError(CategoryService.handleError),
-      map(() => null)
-    );
-  }
-
-  private jsonDataToCategories(jsonData: any[]): CategoryModel[] {
-    const categories: CategoryModel[] = [];
-
-    jsonData.forEach(element => categories.push((element as CategoryModel)));
-    return categories;
-  }
-
-  private jsonDataToCategory(jsonData: any): CategoryModel {
-    return jsonData as CategoryModel;
+export class CategoryService extends BaseResourceService<CategoryModel> {
+  constructor(protected injector: Injector) {
+    super(`${environment.baseUrl}/categories`, injector);
   }
 }
---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,69 @@
+import {BaseResourceModel} from '../models/base-resource.model';
+import {HttpClient} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {Injectable, Injector} from '@angular/core';
+
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+
+  protected http: HttpClient;
+
+  constructor(
+    protected apiPath: string,
+    protected injector: Injector
+  ) {
+    this.http = injector.get(HttpClient);
+  }
+
+  getAll(): Observable<T[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResources)
+    );
+  }
+
+  getById(id: number): Observable<T> {
+    return this.http.get(`${this.apiPath}/${id}`).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    );
+  }
+
+  create(resource: T): Observable<T> {
+    return this.http.post(`${this.apiPath}`, resource).pipe(
+      catchError(this.handleError),
+      map(() => resource)
+    );
+  }
+
+  update(resource: T): Observable<T> {
+    return this.http.put(`${this.apiPath}/${resource.id}`, resource).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToResource)
+    );
+  }
+
+  delete(resource: T): Observable<any> {
+    return this.http.delete(`${this.apiPath}/${resource.id}`).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    );
+  }
+
+  // Protected Methods
+  protected jsonDataToResources(jsonData: any[]): T[] {
+    const resources: T[] = [];
+
+    jsonData.forEach(element => resources.push((element as T)));
+    return resources;
+  }
+
+  protected jsonDataToResource(jsonData: any): T {
+    return jsonData as T;
+  }
+
+  protected handleError(error: any): Observable<any> {
+    console.error(`Erro na requisição -> ${error}`);
+    return throwError(error);
+  }
+}
