7e977f218ac9c411a4bc22a721e224eddf3cf511
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-06-02 12:25:19 -0300

    :zap: improving CategoryListComponent and EntriesListComponent with BaseResourceListComponent

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,49 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {CategoryService} from '../shared/category.service';
 import {CategoryModel} from '../shared/category.model';
-import {Observable} from 'rxjs';
-import {HttpErrorResponse} from '@angular/common/http';
-import {element} from 'protractor';
+import {BaseResourceListComponent} from '../../../shared/components/base-resource-list/base-resource-list-component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.scss']
 })
-export class CategoryListComponent implements OnInit {
-  categories: CategoryModel[] = [];
-
-  constructor(private categoryService: CategoryService) {
-  }
-
-  ngOnInit(): void {
-    this.getAllCategories();
-  }
-
-  getAllCategories(): void {
-    this.categoryService.getAll().subscribe({
-      next: (response: CategoryModel[]) => {
-        this.categories = response;
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error(`Erro ao carregar a lista => ${error.status} | ${error.statusText}`);
-      }
-    });
-  }
-
-  deleteCategory(category: CategoryModel): void {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.categoryService.delete(category).subscribe({
-        next: (response) => {
-          this.categories = this.categories.filter((element: CategoryModel) => element !== category);
-        },
-        error: (error: HttpErrorResponse) => {
-          alert(`Erro ao deletar elemento da lista => ${error.status} | ${error.statusText}`);
-        }
-      });
-    }
+export class CategoryListComponent extends BaseResourceListComponent<CategoryModel> {
+  constructor(protected categoryService: CategoryService) {
+    super(categoryService);
   }
 
 }
--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,47 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {EntryService} from '../shared/entry.service';
 import {EntryModel} from '../shared/entry.model';
-import {HttpErrorResponse} from '@angular/common/http';
+import {BaseResourceListComponent} from '../../../shared/components/base-resource-list/base-resource-list-component';
 
 @Component({
   selector: 'app-entry-list',
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.scss']
 })
-export class EntryListComponent implements OnInit {
-  entries: EntryModel[] = [];
-
-  constructor(private entryService: EntryService) {
+export class EntryListComponent extends BaseResourceListComponent<EntryModel> {
+  constructor(protected entryService: EntryService) {
+    super(entryService);
   }
-
-  ngOnInit(): void {
-    this.getAllEntries();
-  }
-
-  getAllEntries(): void {
-    this.entryService.getAll().subscribe({
-      next: (response: EntryModel[]) => {
-        this.entries = response;
-      },
-      error: (error: HttpErrorResponse) => {
-        console.error(`Erro ao carregar a lista => ${error.status} | ${error.statusText}`);
-      }
-    });
-  }
-
-  deleteEntry(entry: EntryModel): void {
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if (mustDelete) {
-      this.entryService.delete(entry).subscribe({
-        next: (response) => {
-          this.entries = this.entries.filter((element: EntryModel) => element !== entry);
-        },
-        error: (error: HttpErrorResponse) => {
-          alert(`Erro ao deletar elemento da lista => ${error.status} | ${error.statusText}`);
-        }
-      });
-    }
-  }
-
 }
