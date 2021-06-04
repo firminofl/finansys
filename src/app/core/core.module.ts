d434a2aa6e17b58622e8e981ef47394342e42ac1
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-06-04 12:16:25 -0300

    :zap: adding NavBar to AppComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,33 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 import {InMemoryDatabase} from '../in-memory-database';
-
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    RouterModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   exports: [
+    // shared modules
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+
+    // shared components
+    NavbarComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
