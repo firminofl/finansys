4366efc9ab309ced9b08878af7a4b11834e64368
Filipe Firmino <filipe.firmino@leucotron.com.br>
2021-06-07 14:06:07 -0300

    :zap: add HashLocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoreModule} from './core/core.module';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import {CoreModule} from './core/core.module';
     CoreModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    [Location, {provide: LocationStrategy, useClass: HashLocationStrategy}]
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
